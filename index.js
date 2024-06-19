57071eb9d479b2bfa72cf7a57b4a0551c3b48031
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-06-19 13:43:56 -0700

    Add middleware to log request types and send error for unknown endpoints

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from 'express'
 const app = express()
 
-app.use(express.json())
-
 let notes = [
   {
     id: 1,
@@ -20,6 +18,17 @@ let notes = [
     important: true
   }
 ]
+
+const requestLogger = (req, res, next) => {
+  console.log('Method:', req.method)
+  console.log('Path:', req.path)
+  console.log('Body:', req.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -74,6 +83,12 @@ app.post('/api/notes', (req, res) => {
   
   res.json(note)
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
