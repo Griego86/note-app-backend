c2f52b7c32ad29653ac50a40cd818bfb9b30897c
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-06-21 14:47:46 -0700

    Add validation when editing a note in mongoose

--- index.js
+++ index.js
@@ -69,14 +69,12 @@ app.post('/api/notes', (req, res, next) => {
 })
 
 app.put('/api/notes/:id', (req, res, next) => {
-  const body = req.body
+  const { content, important } = req.body
 
-  const note = {
-    content: body.content,
-    important: body.important
-  }
-
-  Note.findByIdAndUpdate(req.params.id, note, { new:true })
+  Note.findByIdAndUpdate(
+    req.params.id, 
+    { content, important },
+    { new:true, runValidators: true, context: 'query' })
     .then(updatedNote => {
       res.json(updatedNote)
     })
