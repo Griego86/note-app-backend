919ff6f447742d76a54eb37d6e9bef03edba44a2
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-06-21 15:13:24 -0700

    Refactor modules to meet MVC standards

---
+++ utils/middleware.js
@@ -0,0 +1,27 @@
+import logger from './logger.js'
+
+const requestLogger = (req, res, next) => {
+  console.log('Method:', req.method)
+  console.log('Path:', req.path)
+  console.log('Body:', req.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({error: 'unknown endpoint'})
+}
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+export default { requestLogger, unknownEndpoint, errorHandler }
