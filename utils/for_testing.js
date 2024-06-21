f261e39be643b2b4c3b07368e6cf0e4e89fb0621
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-06-21 16:03:37 -0700

    Add average tests

--- utils/for_testing.js
+++ utils/for_testing.js
@@ -1,16 +1,18 @@
-const reverse = (string) => {
+const reverse = string => {
   return string
     .split('')
     .reverse()
     .join('')
 }
 
-const average = (array) => {
+const average = array => {
   const reducer = (sum, item) => {
     return sum + item
   }
 
-  return array.reduce(reducer, 0) / array.length
+  return array.length === 0
+    ? 0
+    : array.reduce(reducer, 0) / array.length
 }
 
 export default { reverse, average }
