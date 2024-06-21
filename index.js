4501815297c82cf537d55167c7e85ed95bac7275
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-06-21 14:38:38 -0700

    Add put route to update a single note

--- index.js
+++ index.js
@@ -66,6 +66,21 @@ app.post('/api/notes', (req, res) => {
   })
 })
 
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(req.params.id, note, { new:true })
+    .then(updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
 const unknownEndpoint = (req, res) => {
   res.status(404).send({error: 'unknown endpoint'})
 }
