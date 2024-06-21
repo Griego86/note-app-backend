ee6774a92ebbac5921f25951c388984e51dd19ad
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-06-21 16:25:20 -0700

    Add two more integration tests

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,4 +1,5 @@
 import { test, after } from 'node:test'
+import assert from 'node:assert'
 import mongoose from 'mongoose'
 import supertest from 'supertest'
 import app from '../app.js'
@@ -10,6 +11,19 @@ test('notes are returned as json', async () => {
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+
+  assert.strictEqual(response.body.length, 2)
+})
+
+test('the first note is about HTML', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(e => e.content)
+  assert(contents.includes('HTML'))
 })
 
 after(async () => {
